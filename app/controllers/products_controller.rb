749d4c022c9bf08b771ef8303cf4cb148da36409
Minoryo <ryotafoot@me.com>
2017-04-05 01:43:05 +0900

    viewを編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,9 +5,13 @@ class ProductsController < ApplicationController
   def show
   end
 
+  #new.html.erbを表示
   def new
+    @product = Product.new
   end
 
+  #商品を登録するメソッド
   def regist
+    render :html => "商品名 = #{params[:product][:name]}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'products/index'
+  get 'products', to: 'products#index'
 
   get 'products/show'
 
   get 'products/new'
 
-  get 'products/regist'
+  post 'products/regist'
 
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
